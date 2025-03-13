adf642e3209761df81c656be3965334fe9a93a23
Henzwee <166328821+Henzwee@users.noreply.github.com>
2025-03-13 00:22:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const rect = text.parentElement.getBoundingClientRect();
             const windowHeight = window.innerHeight;
             
-            if (rect.top < windowHeight * 0.75 && rect.bottom > windowHeight * 0.25) {
+            if (rect.top < windowHeight * 0.75 && rect.bottom > 0) {
                 text.style.opacity = '1';
             } else {
                 text.style.opacity = '0';
